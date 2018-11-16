975a9dbdb26a7dbea3e8df00ab35e0457ecce396
shgfl007 <shgfl@hotmail.com>
2018-11-16 00:02:32 -0500

    add navbar scroll

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import classnames from 'classnames'
 
 export default class Nav extends Component {
+  constructor () {
+    super()
+    this.state = {
+      menuOpen: false,
+      solutionsOpen: false,
+      verticalPosition: 'top'
+    }
+    this.onScroll = this.onScroll.bind(this)
+  }
+
+  componentDidMount () {
+    window.addEventListener('scroll', this.onScroll)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('scroll', this.onScroll)
+  }
+
+  onScroll () {
+    const fadeCutoff = 320
+    const scrolly = window.scrollY
+    const { verticalPosition } = this.state
+
+    const position = scrolly < fadeCutoff ? 'top' : 'bottom'
+    if (position === verticalPosition) return
+    this.setState({ verticalPosition: position })
+  }
+
+  getNavClass () {
+    const { verticalPosition } = this.state
+    const isGrey = verticalPosition === 'bottom'
+    return classnames('nav-home', { 'nav-small': isGrey })
+  }
+
 
   render() {
+    const className = this.getNavClass()
     return (
-      <nav className='nav-home'>
+      <nav className={className}>
           <ul className='list'>
                   <li className='item'>
                     <Link to='/'>Home</Link>
