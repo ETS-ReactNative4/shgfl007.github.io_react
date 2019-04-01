fb0c080d04d1a13763faa56cd8382968d68cc019
shgfl007 <shgfl@hotmail.com>
2019-03-31 23:52:08 -0400

    update and add hotjar changed header section

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -17,7 +17,7 @@ export default class Header extends Component {
 
             </div>*/}
             <div className='header-split-right'>
-            <h2 className="title-main-header">Hi, I'm Danning Lu. An interaction designer based in Toronto, Canada.</h2>
+            <h2 className="title-main-header">Hi, I'm Danning Lu. <br/>An interaction designer based in Toronto, Canada.</h2>
               <p className='summary'>With a background in programming and entrepreneurship, my strength lies in research & rapid prototyping for web & mobile apps. During my career I had the privilege to work with companies that are challenging the new territory of AR and VR. </p>
             </div>
           </div>
