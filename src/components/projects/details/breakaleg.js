9ef8f4fcd966bec441016525a39d35e3d06f89f1
shgfl007 <shgfl@hotmail.com>
2019-03-31 20:56:03 -0400

    update

--- src/components/projects/details/breakaleg.js
+++ src/components/projects/details/breakaleg.js
@@ -11,40 +11,12 @@ export default class FairTrade extends Component {
             <h1 className='title'>Paper Prototype for Quick Play Test</h1>
             <hr/>
             <h3>Break A Leg!</h3>
-            <p>A party game that brings fun to all the players</p>
           </div>
           <div className='project-content animated'>
             <p> </p>
 
-              <h3>Team and Roles</h3>
-              <p>In this project I worked as a UX designer collaboration
-              with another UI/UX designer and later on I used Unity to build
-              a working prototype in Angel Hack 2018 Toronto. After winning
-              the hackathon, I continued working on Fair Trade to shape the
-              branding and user experience. </p>
+              <h3>WIP</h3>
 
-              <div className='section-block'>
-                <h3>Challenge</h3>
-                <p>The challenge for this project was to discover what we can
-                create to bridge the gap between new comers and locals to help
-                 them adapt and </p>
-              </div>
-
-
-
-            <h3>Discovery</h3>
-            <p></p>
-
-            <h3>Prototyping and tests</h3>
-            <p></p>
-            <p>
-            </p>
-
-
-            <h3>Detailed Design</h3>
-
-
-            <h3>Takeaways</h3>
 
           </div>
     </div>
